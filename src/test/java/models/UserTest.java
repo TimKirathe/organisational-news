a12369e84a3b30e820018b77f2c86cb64d707d98
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-01-16 19:02:26 +0300

    Created two working tests for instantiation of the User class and getter methods working correctly.

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -1,7 +1,26 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
+
+    @Test
+    void userInstantiatedCorrectly() {
+        User testUser = setUpUser();
+        assertTrue(testUser instanceof User);
+    }
+
+    @Test
+    void gettersWorkCorrectly() {
+        User testUser = setUpUser();
+        assertEquals("Timothy", testUser.getName());
+    }
+
+    public User setUpUser() {
+        User testUser = new User("Timothy", "Tech", "Employee", 1);
+        return testUser;
+    }
 
 }
