05d6893533f69a5148f6e396b1b3d8baf487bd89
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-01-18 00:14:39 +0300

    Created pom.xml file for deploying to heroku and created README file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,8 +18,16 @@ import static spark.Spark.*;
 import static spark.route.HttpMethod.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
 
         Sql2oDepartmentDao departmentDao;
         Sql2oNewsDao newsDao;
