d0f691e75b63f8062ad951872d30aed9362fb83b
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-01-17 08:01:16 +0300

    Created a working test for these methods.

--- src/test/java/dao/Sql2oUserDaoTest.java
+++ src/test/java/dao/Sql2oUserDaoTest.java
@@ -1,7 +1,48 @@
 package dao;
 
+import models.User;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Sql2oUserDaoTest {
+    private static Sql2oUserDao sql2oUserDao;
+    private static Connection conn;
+
+    @BeforeAll
+    static void setUp() {
+        String connectionString = "jdbc:postgresql://localhost:5432/organisational_news_test";
+        Sql2o sql2o = new Sql2o(connectionString, null, null);
+        sql2oUserDao = new Sql2oUserDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @AfterEach
+    void afterEach() {
+        sql2oUserDao.clearAll();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        conn.close();
+    }
+
+    @Test
+    void userSavedAndReturnedFromDb() {
+        User testUser = setUpUser();
+        sql2oUserDao.save(testUser);
+        User savedUser = sql2oUserDao.findById(testUser.getId());
+        assertTrue(testUser.equals(savedUser));
+    }
+
+    public User setUpUser() {
+        User testUser = new User("Timothy", 1, "Employee", 1);
+        return testUser;
+    }
 
 }
